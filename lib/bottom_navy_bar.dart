6236d3529e9cd8eefed8dace2724c1d696731f2d
Ferhat Sayan <fs@nitash.dev>
2021-08-26 18:10:19 +0200

    fixed bottom navy bar for o ur project

--- lib/bottom_navy_bar.dart
+++ lib/bottom_navy_bar.dart
@@ -132,24 +132,23 @@ class _ItemWidget extends StatelessWidget {
       container: true,
       selected: isSelected,
       child: AnimatedContainer(
-        width: isSelected ? item.selectedWidth : item.width,
+        width: item.width,
         height: double.maxFinite,
         duration: animationDuration,
         curve: curve,
         decoration: BoxDecoration(
-          color:
-              isSelected ? item.activeColor.withOpacity(0.2) : backgroundColor,
+          color: isSelected ? item.activeColor.withOpacity(0.2) : backgroundColor,
           borderRadius: BorderRadius.circular(itemCornerRadius),
         ),
         child: SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           physics: NeverScrollableScrollPhysics(),
           child: Container(
-            width: isSelected ? item.selectedWidth : item.width,
+            width: item.width,
             padding: EdgeInsets.symmetric(horizontal: 8),
             child: Row(
               mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 IconTheme(
@@ -163,21 +162,6 @@ class _ItemWidget extends StatelessWidget {
                   ),
                   child: item.icon,
                 ),
-                if (isSelected)
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.symmetric(horizontal: 4),
-                      child: DefaultTextStyle.merge(
-                        style: TextStyle(
-                          color: item.activeColor,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        maxLines: 1,
-                        textAlign: item.textAlign,
-                        child: item.title,
-                      ),
-                    ),
-                  ),
               ],
             ),
           ),
@@ -191,35 +175,21 @@ class _ItemWidget extends StatelessWidget {
 class BottomNavyBarItem {
   BottomNavyBarItem({
     required this.icon,
-    required this.title,
     this.activeColor = Colors.blue,
-    this.selectedWidth = 130,
     this.width = 50,
-    this.textAlign,
     this.inactiveColor,
   });
 
   /// Defines this item's icon which is placed in the right side of the [title].
   final Widget icon;
 
-  /// Defines this item's title which placed in the left side of the [icon].
-  final Widget title;
-
   /// The [icon] and [title] color defined when this item is selected. Defaults
   /// to [Colors.blue].
   final Color activeColor;
-  
-  /// Defines the selected item width. Defaults to 130.
-  final double selectedWidth;
-  
+
   /// Defines the item width. Defaults to 50.
   final double width;
 
   /// The [icon] and [title] color defined when this item is not selected.
   final Color? inactiveColor;
-
-  /// The alignment for the [title].
-  ///
-  /// This will take effect only if [title] it a [Text] widget.
-  final TextAlign? textAlign;
 }
